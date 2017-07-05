9661ce0054ea43855fc10523326999ee09902914
joshlacey <williamjoshualacey@gmail.com>
2017-07-05 15:15:38 -0400

    done

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -13,8 +13,19 @@ def first_challenge
     }
   }
 
-  #your code here
-
+  contacts.each do |person, data|
+      data.each do |attribute, value|
+          if attribute == :favorite_icecream_flavors
+              value.each do |flavor|
+                  if flavor == "strawberry"
+                      value.delete(flavor)
+                      end
+                  puts "#{flavor}"
+                end
+              end
+          end
+      end
+  
 
   #remember to return your newly altered contacts hash!
   contacts
